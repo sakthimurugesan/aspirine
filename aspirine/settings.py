9d40cd05307969747e4fe00838c18f19a3548210
SAKTHI M <msakthi150@gmail.com>
2024-05-01 21:58:31 +0530

    project complete

--- aspirine/settings.py
+++ aspirine/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aspirine',
-        'USER': 'sakthi',
+        'USER': 'root',
         'PASSWORD': 'password',
-        'HOST':'103.168.18.136',
+        'HOST':'localhost',
         'PORT':'3306',
     }
 }
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE =  'Asia/Kolkata'
 
 USE_I18N = True
 
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import M,Coordinates,OffSwitch,OffTable
+import schedule
 import time as t
 from datetime import *
+import requests
+import threading
+import pytz
+
 
 
 def home(request):
     m=M.objects.values().all()
+    d=datetime.now()
+    t2=timedelta(minutes=2)
+    print(type(d-t2))
     return render(request,template_name="index.html",context={"m":m})
 
 def getdata(request):
     lat=request.GET['lat']
     lng=request.GET['lng']
+    m=M(s=lat,s2=lng)
+    m.save()
     
     coords=Coordinates.objects.all()
     for coor in coords:
@@ -28,6 +38,7 @@ def getdata(request):
         if(is_point_inside_rectangle(x1,y1,x2,y2,x,y)):
             laneId=coor.lane.id
             lane1_on_url=coor.lane.green_on_url #green on url
+            requests.get(url=lane1_on_url)
             print(lane1_on_url)
             lane1_off_url=coor.lane.green_off_url # green off url 
             check_lane_exist_in_offtable=OffTable.objects.filter(lane=coor.lane)
@@ -39,8 +50,11 @@ def getdata(request):
                 for i in temp:
                     print(i.offswitch.red_switch_on_url) # red on url
                     url_list.append(i.offswitch.red_switch_off_url)
-                
-                d=datetime.now()+timedelta(minutes=2)
+                d1=datetime.now()
+                d=datetime(d1.year,d1.month,d1.day,d1.hour,d1.minute)+timedelta(minutes=2)
+
+                print("ON TIME ",datetime.now())
+                print("OFF TIME",d)
 
                 for i in url_list:
                     offTableObj=OffTable(lane=coor.lane,offURL=i,offtime=d)
@@ -49,12 +63,6 @@ def getdata(request):
                 for obj in check_lane_exist_in_offtable:
                     obj.offtime+=timedelta(minutes=2)
                     obj.save()
-
-
-
-            
-
-
         else:
             pass
     return HttpResponse("<h1>"+lat+" "+lng+"</h1>")
@@ -63,3 +71,27 @@ def is_point_inside_rectangle(x1, y1, x2, y2, x, y):
     
     return x1 <= x <= x2 and y1 <= y <= y2
 
+
+def sayHi():
+    k=1
+    while True:
+        print("inside function")
+        d1=datetime.now()
+        d=datetime(d1.year,d1.month,d1.day,d1.hour,d1.minute,0)
+        offtable_data=OffTable.objects.all()
+
+        for obj in offtable_data:
+            temp_date=datetime(obj.offtime.year,obj.offtime.month,obj.offtime.day,obj.offtime.hour,obj.offtime.minute,0)
+            if(temp_date<=d):
+                try:
+                    requests.get(obj.offURL)
+                except:
+                    pass
+
+                obj.delete()       
+        t.sleep(60)
+
+
+
+my_thread = threading.Thread(target=sayHi)
+my_thread.start()
